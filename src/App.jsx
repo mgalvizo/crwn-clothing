2dbf29d104b153bec417c075c3bd9dd0888e1bed
Miguel Garcia <miguelangelgarciaalvizo@hotmail.com>
2023-07-24 17:55:56 -0600

    add uuid, sass, categories and category-item components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,36 @@
+import { v4 as uuidv4 } from 'uuid';
+import Directory from './components/directory/Directory.component';
+
+const categories = [
+    {
+        id: uuidv4(),
+        title: 'hats',
+        imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+    },
+    {
+        id: uuidv4(),
+        title: 'jackets',
+        imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+    },
+    {
+        id: uuidv4(),
+        title: 'sneakers',
+        imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+    },
+    {
+        id: uuidv4(),
+        title: 'womens',
+        imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+    },
+    {
+        id: uuidv4(),
+        title: 'mens',
+        imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+    },
+];
+
 const App = () => {
-    return <div>App</div>;
+    return <Directory categories={categories} />;
 };
 
 export default App;
