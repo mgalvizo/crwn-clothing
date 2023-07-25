1f45bd7ebacb97a8a6fd74be3f6fbdbcc3e5efb2
Miguel Garcia <miguelangelgarciaalvizo@hotmail.com>
2023-07-25 12:21:58 -0600

    add react router and first route

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,12 @@
-import { v4 as uuidv4 } from 'uuid';
-import Directory from './components/directory/Directory.component';
-
-const categories = [
-    {
-        id: uuidv4(),
-        title: 'hats',
-        imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-    },
-    {
-        id: uuidv4(),
-        title: 'jackets',
-        imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-    },
-    {
-        id: uuidv4(),
-        title: 'sneakers',
-        imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-    },
-    {
-        id: uuidv4(),
-        title: 'womens',
-        imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-    },
-    {
-        id: uuidv4(),
-        title: 'mens',
-        imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-    },
-];
+import { Routes, Route } from 'react-router-dom';
+import Home from './routes/home/home.component';
 
 const App = () => {
-    return <Directory categories={categories} />;
+    return (
+        <Routes>
+            <Route path="/" index element={<Home />} />
+        </Routes>
+    );
 };
 
 export default App;
